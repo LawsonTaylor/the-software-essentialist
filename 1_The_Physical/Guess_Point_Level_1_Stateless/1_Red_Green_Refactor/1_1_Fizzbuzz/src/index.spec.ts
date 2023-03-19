9c4474063d79215ddf9f6c3dadb1d61973a6a10b
LawsonTaylor <lawson.j.taylor@gmail.com>
2023-03-19 10:48:51 -0600

    [TEST] - fizzbuzz() returns Buzz for multiples of 5

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -20,6 +20,18 @@ describe("fizzbuzz", () => {
         expect(fizzbuzz(9)).toBe("Fizz");
     });
 
+    it("should return 'Buzz' for multiples of 5", () => {
+        expect(fizzbuzz(5)).toBe("Buzz");
+        expect(fizzbuzz(10)).toBe("Buzz");
+        expect(fizzbuzz(20)).toBe("Buzz");
+    })
+
+    it("should return 'FizzBuzz' for multiples of 3 and 5", () => {
+        expect(fizzbuzz(15)).toBe("FizzBuzz");
+        expect(fizzbuzz(30)).toBe("FizzBuzz");
+        expect(fizzbuzz(45)).toBe("FizzBuzz");
+    });
+
     it("should return the number as a string for inputs that are not a multiple of 3 or 5", () => {
         expect(fizzbuzz(1)).toBe("1");
         expect(fizzbuzz(2)).toBe("2");
