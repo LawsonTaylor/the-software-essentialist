04d27605a42b5073f7a33112a4a7e5fba4ae25c9
LawsonTaylor <lawson.j.taylor@gmail.com>
2023-03-19 10:30:23 -0600

    [TEST] - fizzbuzz() returns a string

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,9 @@
+import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
 
+    it("should return a string", () => {
+        expect(typeof fizzbuzz(5)).toBe("string");
+    });
 
 });
